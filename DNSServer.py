aa5bf8e743dc073583117280583e52f7a96e9f5a
fakirpoo <74319215+fakirpoo@users.noreply.github.com>
2023-10-28 07:07:34 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -65,7 +65,7 @@ dns_records = {
     'example.com.': {
         dns.rdatatype.A: '192.168.1.101',
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0370:7334',
-       # dns.rdatatype.MX: [(10, 'mail.example.com.')],  # List of (preference, mail server) tuples
+        dns.rdatatype.MX: [(10, 'mail.example.com.')],  # List of (preference, mail server) tuples
         dns.rdatatype.CNAME: 'www.example.com.',
         dns.rdatatype.NS: 'ns.example.com.',
         dns.rdatatype.TXT: ('This is a TXT record',),
