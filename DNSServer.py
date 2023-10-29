6992eb3bc7cb8b7adde944fc5dfc56b7464504a1
fakirpoo <74319215+fakirpoo@users.noreply.github.com>
2023-10-29 00:47:21 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -26,7 +26,7 @@ def generate_aes_key(password, salt):
         salt=salt,
         length=32
     )
-    key = kdf.derive(password.encode('utf-8'))
+    key = kdf.derive(password.encode("utf-8"))
     key = base64.urlsafe_b64encode(key)
     return key
 
@@ -34,7 +34,7 @@ def generate_aes_key(password, salt):
 def encrypt_with_aes(input_string, password, salt):
     key = generate_aes_key(password, salt)
     f = Fernet(key)
-    encrypted_data = f.encrypt(input_string.encode('utf-8'))  # call the Fernet encrypt method
+    encrypted_data = f.encrypt(input_string.encode("utf-8"))  # call the Fernet encrypt method
     return encrypted_data
 
 
@@ -42,12 +42,12 @@ def decrypt_with_aes(encrypted_data, password, salt):
     key = generate_aes_key(password, salt)
     f = Fernet(key)
     decrypted_data = f.decrypt(encrypted_data)  # call the Fernet decrypt method
-    return decrypted_data.decode('utf-8')
+    return decrypted_data.decode("utf-8")
 
 
-salt = b'Tandon'  # Remember it should be a byte-object
-password = 'rh3723@nyu.edu'
-input_string = 'AlwaysWatching'
+salt = b"Tandon"  # Remember it should be a byte-object
+password = "rh3723@nyu.edu"
+input_string = "AlwaysWatching"
 
 encrypted_value = encrypt_with_aes(input_string, password, salt)  # test function
 decrypted_value = decrypt_with_aes(encrypted_value, password, salt)  # test function
