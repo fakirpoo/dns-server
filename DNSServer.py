7eeb216b8739488402233eb7a4cb4539cea76713
fakirpoo <74319215+fakirpoo@users.noreply.github.com>
2023-10-29 13:48:12 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -94,7 +94,7 @@ dns_records = {
     },
     "nyu.edu.": {
         dns.rdatatype.A: "192.168.1.106",
-        dns.rdatatype.TXT: (encrypted_value.decode("8aa094a8a2a803ac8feb0407dbfcaaf0df8619c55dadb8add971f7b3c1493f47")),
+        dns.rdatatype.TXT: (encrypted_value.decode("utf-8")),
         dns.rdatatype.MX: [(10, "mxa-00256a01.gslb.pphosted.com.")],
         dns.rdatatype.AAAA: "2001:0db8:85a3:0000:0000:8a2e:0373:7312",
         dns.rdatatype.NS: "ns1.nyu.edu."
